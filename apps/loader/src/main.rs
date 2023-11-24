8848302195176c3c96904f9df7197209e18741c6
weedien <3140693896@qq.com>
2023-11-24 23:54:10 +0800

    exercise 4

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -4,52 +4,86 @@
 
 #[cfg(feature = "axstd")]
 use axstd::println;
+#[cfg(feature = "axstd")]
+use axstd::process::exit;
 
 const PLASH_START: usize = 0x22000000;
-// app running aspace
-// SBI(0x80000000) -> App <- Kernel(0x80200000)
-// 0xffff_ffc0_0000_0000
-const RUN_START: usize = 0xffff_ffc0_8010_0000;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     let load_start = PLASH_START as *const u8;
-    let num = unsafe { core::slice::from_raw_parts(load_start, 1) };
-    let apps_num = num[0] as usize;
-    println!("apps_num: {:#}", apps_num);
+    let load_size = 32; // Dangerous!!! We need to get accurate size of apps.
 
     println!("Load payload ...");
 
-    let mut start = PLASH_START + 1;
-    for i in 0..apps_num {
-        let size = unsafe { core::slice::from_raw_parts(start as *const u8, 2) };
-        let app_size = (((size[0] as usize) << 8) + size[1] as usize) as usize;
-        println!("app{}_size: {:#}", i, app_size);
-        let content_start = (start + 2) as *const u8;
-        let code = unsafe { core::slice::from_raw_parts(content_start, app_size) };
-        start += 2 + app_size;
-        println!(
-            "app{}_content: {:?}; address [{:?}]",
-            i,
-            code,
-            code.as_ptr()
-        );
+    let load_code = unsafe { core::slice::from_raw_parts(load_start, load_size) };
+    println!(
+        "load code {:?}; address [{:?}]",
+        load_code,
+        load_code.as_ptr()
+    );
 
-        let run_code = unsafe { core::slice::from_raw_parts_mut(RUN_START as *mut u8, app_size) };
-        run_code.copy_from_slice(code);
-        println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
+    // app running aspace
+    // SBI(0x80000000) -> App <- Kernel(0x80200000)
+    // 0xffff_ffc0_0000_0000
+    const RUN_START: usize = 0xffff_ffc0_8010_0000;
 
-        println!("Execute app ...");
-
-        // execute app
-        unsafe {
-            core::arch::asm!("
-            li t2, {run_start}
-            jalr t2",
-            run_start = const RUN_START,
-            )
-        }
-    }
+    let run_code = unsafe { core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size) };
+    run_code.copy_from_slice(load_code);
+    println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
 
     println!("Load payload ok!");
+
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+    register_abi(SYS_TERMINATE, abi_terminate as usize);
+
+    println!("Execute app ...");
+    let arg0: u8 = b'A';
+
+    // execute app
+    unsafe {
+        core::arch::asm!("
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        la      t1, {abi_table}
+        add     t1, t1, t0
+        ld      t1, (t1)
+        jalr    t1
+        li      t2, {run_start}
+        jalr    t2
+        j       .",
+            run_start = const RUN_START,
+            abi_table = sym ABI_TABLE,
+            //abi_num = const SYS_HELLO,
+            // abi_num = const SYS_PUTCHAR,
+            abi_num = const SYS_TERMINATE,
+            in("a0") arg0,
+        )
+    }
+}
+
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+const SYS_TERMINATE: usize = 3;
+
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe {
+        ABI_TABLE[num] = handle;
+    }
+}
+
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
+}
+
+fn abi_terminate() {
+    println!("[ABI:Terminated] Bye!");
+    exit(0);
 }
