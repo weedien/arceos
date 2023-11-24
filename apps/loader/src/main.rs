cc917692ac1803c8c264df8b4248c2dd6822e669
weedien <3140693896@qq.com>
2023-11-24 22:13:47 +0800

    exercise 2

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -9,21 +9,27 @@ const PLASH_START: usize = 0x22000000;
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     let apps_start = PLASH_START as *const u8;
-    let app_size = unsafe { core::slice::from_raw_parts(apps_start, 2) };
-    let app_size = (((app_size[0] as usize) << 8) + app_size[1] as usize) as usize;
-
-    println!("app_size: {:#}", app_size);
+    let num = unsafe { core::slice::from_raw_parts(apps_start, 1) };
+    let apps_num = num[0] as usize;
+    println!("apps_num: {:#}", apps_num);
 
     println!("Load payload ...");
 
-    let start = PLASH_START + 2;
-    let code = unsafe { core::slice::from_raw_parts(start as *const u8, app_size) };
-    println!("content: {:?}", code);
+    let mut start = PLASH_START + 1;
+    for i in 0..apps_num {
+        let size = unsafe { core::slice::from_raw_parts(start as *const u8, 2) };
+        let app_size = (((size[0] as usize) << 8) + size[1] as usize) as usize;
+        println!("app{}_size: {:#}", i, app_size);
+        let content_start = (start + 2) as *const u8;
+        let code = unsafe { core::slice::from_raw_parts(content_start, app_size) };
+        start += 2 + app_size;
+        println!("app{}_content: {:?}", i, code);
+    }
 
     println!("Load payload ok!");
 }
 
-#[inline]
-fn bytes_to_usize(bytes: &[u8]) -> usize {
-    usize::from_be_bytes(bytes.try_into().unwrap())
-}
+// #[inline]
+// fn bytes_to_usize(bytes: &[u8]) -> usize {
+//     usize::from_be_bytes(bytes.try_into().unwrap())
+// }
